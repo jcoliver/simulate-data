54d771a8e6649a78021d9276dd033a23451b755f
Jeff Oliver <jcoliver@arizona.edu>
2025-10-21 10:11:21 -0700

    Initial commit

---
+++ simulate-plot-data.R
@@ -0,0 +1,11 @@
+# Simulate and plot data
+# Jeff Oliver
+# jcoliver@arizona.edu
+# 2025-10-21
+
+# Simulate predictor variable
+predictor <- rnorm(n = 100)
+# Simulate response variable with noise
+response <- 2 * predictor + rnorm(n = 100, sd = 0.2)
+# Plot the data
+plot(x = predictor, y = response)
